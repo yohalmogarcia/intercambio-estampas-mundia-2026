0627dd4241babc8672badae0e631c51854706997
Yohalmo Garcia <yohalmo.garcia@gmail.com>
2026-05-11 22:45:41 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: {
-    appIsrStatus: false, // Quita el indicador de estatus de renderizado
-    buildActivity: false, // Quita el indicador de compilación
   },
 };
 
